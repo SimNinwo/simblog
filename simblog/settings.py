831e81ce734a4c2f64fa40d4d6cdb49394de155e
Simeon Olorunninwo <simeontoluwanimi@gmail.com>
2019-11-06 15:44:46 +0100

    new changes

--- simblog/settings.py
+++ simblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i3du_p2i%efzywnr(19qp%$!3#t1jw(@u21g%b_odsa)oz#iti'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
